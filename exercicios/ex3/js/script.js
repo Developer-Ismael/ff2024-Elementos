dca51ce9a4e972dff099945b3e325df12cc13247
ismael.pvlsilva <ismael.pvlsilva@senacsp.edu.br>
2024-09-30 22:18:23 -0300

    exercicios

---
+++ exercicios/ex3/js/script.js
@@ -0,0 +1,5 @@
+// Faça um algoritmo para a leitura de duas notas parciais de um aluno. O algoritmo deve calcular a média
+// alcançada por aluno e apresentar:
+// 3. A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+// 4. A mensagem "Reprovado", se a média for menor do que cinco;
+// 5. A mensagem “Recuperação", se a média estiver entre cinco e sete.
