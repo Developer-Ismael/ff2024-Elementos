21cddd52c099f5ac32790b04acd50c6cd37cd379
ismael.pvlsilva <ismael.pvlsilva@senacsp.edu.br>
2024-10-01 21:10:17 -0300

    feat: Ex4 Corrigido

--- exercicios/ex4/js/script.js
+++ exercicios/ex4/js/script.js
@@ -6,3 +6,31 @@
 // 5. Quinta-feira
 // 6. Sexta-feira
 // 7. Sábado
+
+function exibirSemana() {
+    let dSemana = document.querySelector("#diaSemana").value;
+    let resultado = "";
+    if (dSemana == "1") 
+        resultado = "Domingo"
+    else if (dSemana == "2")
+        resultado = "Segunda-Feira"
+    else if (dSemana == "3")
+        resultado = "Terça-Feira"
+    else if (dSemana == "4")
+        resultado = "Quarta-Feira"
+    else if (dSemana == "5")
+        resultado = "Quinta-Feira"
+    else if (dSemana == "6")
+        resultado = "Sexta-Feira"
+    else if (dSemana == "7")
+        resultado = "Sábado"
+    else 
+        resultado = "Dia Invalido";
+
+    let p = document.querySelector("p");
+    p.innerHTML = resultado;
+}
+
+document
+    .querySelector('button')
+    .addEventListener('click', exibirSemana)
