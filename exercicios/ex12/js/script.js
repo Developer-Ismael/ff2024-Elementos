f0a255e2fb87ff78453a3821787a2275f35dae63
ismael.pvlsilva <ismael.pvlsilva@senacsp.edu.br>
2024-10-03 20:58:50 -0300

    feat: Ex12

--- exercicios/ex12/js/script.js
+++ exercicios/ex12/js/script.js
@@ -7,3 +7,36 @@
 // 4. Divisão
 // 5. Potenciação
 // 6. Radiciação
+
+function calcular() {
+    let numero1 = parseFloat(document.querySelector('.nu1').value);
+    let numero2 = parseFloat(document.querySelector('.nu2').value);
+    let operacao = document.querySelector('select').value;
+
+
+    switch (operacao) {
+        case "+":
+            document.querySelector('.resultado').innerHTML = ` A soma é ${numero1 + numero2}`;
+            break;
+        case "-":
+            document.querySelector('.resultado').innerHTML = ` A subtração é ${numero1 - numero2}`;
+            break;
+        case "*":
+            document.querySelector('.resultado').innerHTML = ` A multiplicação é ${numero1 * numero2}`;
+            break;
+        case "/":
+            document.querySelector('.resultado').innerHTML = ` A divisão é ${numero1 / numero2}`;
+            break;
+        case "**":
+            document.querySelector('.resultado').innerHTML = `A potenciação é ${numero1 ** numero2}`;
+            break;
+        case "|":
+            document.querySelector('.resultado').innerHTML = ` A radiciação é ${numero1 ** (1/numero2)}`;
+            break;
+    }
+
+}
+
+    document
+        .querySelector('button')
+        .addEventListener('click', calcular);
