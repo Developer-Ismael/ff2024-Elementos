81580337ec3f65398982ddbdb10b9ac7ad754e92
ismael.pvlsilva <ismael.pvlsilva@senacsp.edu.br>
2024-10-03 22:16:59 -0300

    feat: Ex13 Corrigido

--- exercicios/ex13/js/script.js
+++ exercicios/ex13/js/script.js
@@ -1,3 +1,24 @@
 // Faça um algoritmo que faz uma conversão de temperaturas onde . 
 // 1. Converter Celsius para Fahrenheit. formula: (celsius * 9/5) + 32
 // 2. Converter Fahrenheit para Celsius. formula: (fahrenheit - 32) * 5/9
+
+
+function conversaotemp() {
+    let temperatura = parseFloat(document.querySelector('#temperatura').value);
+    let op = document.querySelector('#operador').value;
+
+    switch (op) {
+        case "1":
+            document.querySelector('.resultado').innerHTML = ` A Temperatura de ${temperatura} ºC equivale a ${((temperatura * 9 / 5) + 32)}ºF`;
+            break;
+        case "2":
+            document.querySelector('.resultado').innerHTML = ` A Temperatura de ${temperatura} ºF equivale a  ${((temperatura - 32) * 5 / 9)}ºC`;
+            break;
+
+    }
+
+}
+
+document
+    .querySelector('button')
+    .addEventListener('click', conversaotemp);
