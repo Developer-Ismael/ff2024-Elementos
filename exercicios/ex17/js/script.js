55af54a1991e144e26229499b375d02758c6aba9
ismael.pvlsilva <ismael.pvlsilva@senacsp.edu.br>
2024-10-08 21:10:05 -0300

    feat: Ex17 Corrigido

--- exercicios/ex17/js/script.js
+++ exercicios/ex17/js/script.js
@@ -1,9 +1,76 @@
-// O Sr. Manoel Joaquim acaba de adquirir uma panificadora e pretende implantar a metodologia
-// da tabelinha. Você foi contratado para desenvolver o programa que monta a tabela de preços
-// de pães, de 1 até 50 pães, a partir do preço do pão informado pelo usuário, conforme o
-// exemplo abaixo:
-// Preço do pão: R$ 0,25 unid.
-//  1 = R$ 0,25
-//  2 = R$ 0,50
+// Desenvolva um gerador de tabuada, capaz de gerar através de qualquer número inteiro, 
+// solicitando ao usuário o número e mostrando na tela conforme o exemplo abaixo:
+// Tabuada de 5:
+// 5 x 1 = 5
+// 5 x 2 = 10
 // ...
-// 50 = R$ 12,50
+// 5 x 10 = 50
+// Ao final, pergunte ao usuário se ele deseja realizar uma outra geração ou sair.
+
+
+// function tabelaPao() {
+//     let numero = document.querySelector('#numero').value;
+
+//     if (isNaN(numero) == true || numero == "") {
+//         alert("Número Inválido!");
+//         document.querySelector('#numero').value = "";
+//     } else {
+//         let msgPao = "";
+//         for (let i = 1; i <= 50; i++) {
+
+//             let ordem = (i === 1) ? "Pão" : "Pães";
+//             msgPao += ` ${i} ${ordem} = R$ ${(numero * i).toFixed(2)} <br>`;
+//         }
+//         document.querySelector('h5').innerHTML = msgPao;
+//         document.querySelector('#numero').disabled = true;
+//         document.querySelector('.btn-primary').disabled = true;
+//         document.querySelector('.btn-dark').disabled = false;
+//     }
+
+
+// }
+// function liberarTabuada() {
+//     let podeGerar = confirm("Deseja gerar outra tabela de preço?");
+
+//     if (podeGerar == true) {
+//         document.querySelector('#numero').disabled = false;
+//         document.querySelector('.btn-primary').disabled = false;
+//         document.querySelector('.btn-dark').disabled = true;
+//     }
+// }
+// document
+//     .querySelector('.btn-primary')
+//     .addEventListener('click', tabelaPao);
+
+// document
+//     .querySelector('.btn-dark')
+//     .addEventListener('click', liberarTabuada);
+
+function gerarTabela() {
+    let preco = document.querySelector('#preco').value;
+    preco = preco.replace(',','.');
+    if (isNaN(preco) || preco == '') {
+        alert("Valor inválido!");
+    }
+    else {
+        preco = parseFloat(preco);
+        let htmlPreco = "";
+        for (let i = 1; i <= 50; i++) {
+            if (i == 1) {
+                htmlPreco += `
+                    <tr><td>1 pão</td>
+                        <td>R$ ${preco.toFixed(2)}</td>
+                    </tr>`;
+            } else {
+                htmlPreco += `
+                    <tr><td>${i} pães</td>
+                        <td>R$ ${(preco * i).toFixed(2)}</td>
+                    </tr>`;
+            }
+        }
+        document.querySelector('.tabelaPreco').innerHTML = htmlPreco;
+    }
+}
+document
+    .querySelector('.btn-danger')
+    .addEventListener('click', gerarTabela);
